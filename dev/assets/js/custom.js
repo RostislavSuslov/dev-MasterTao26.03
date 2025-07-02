d88b7a08194eba0e16bafbc38304d6c758b70a8d
RostSuslov <suslovros@gmail.com>
2025-07-02 20:42:42 +0300

    feat: adoptation slided for mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,21 +1,20 @@
 const btnBurger = document.querySelector('.hamburger')
-btnBurger.addEventListener('click', ()=> btnBurger.classList.toggle('is-active'))
+btnBurger.addEventListener('click', () => btnBurger.classList.toggle('is-active'))
 
 const swiperBanner = new Swiper('.swiper-banner', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  spaceBetween: 24,
-  // If we need pagination
-  
-  pagination: {
-    clickable: true,
-    el: '.swiper-pagination',
-  },
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 24,
+    // If we need pagination
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    pagination: {
+        clickable: true,
+        el: '.swiper-pagination',
+    },
 });
